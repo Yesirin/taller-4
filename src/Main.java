4359b9f50539bba92d633589bbea12d29b063ffe
yrinco2 <ing.yesidrincon@gmail.com>
2024-08-09 21:41:15 -0500

    taller-4.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String [] args) {
+       int suma = 0 ;
+
+ //ejercicio para el FOR
+
+       /* for (int i= 1; i <= 10; i++) {
+            suma += i;
+        }
+        System.out.println(" la suma con el for es:" + suma);*/
+
+
+ // ejercicio para el  while
+
+
+    int i = 1;
+    while (i <= 10){
+        suma += i;
+        i++;
     }
+
+        System.out.println("la suma con el while es:" + suma);
+    }
+
 }
